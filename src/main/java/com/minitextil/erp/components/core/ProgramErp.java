17ed88a92f76ebf017993c0377308119114d3af1
FelipeVieira4 <felipevieira58238@gmail.com>
2026-08-07 21:15:52 -0300

    alteração: nome classe padrão de programaas doERP

--- src/main/java/com/minitextil/erp/components/core/ProgramErp.java
+++ src/main/java/com/minitextil/erp/components/core/ProgramErp.java
@@ -4,10 +4,10 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 
-public class ErpProgram extends VerticalLayout implements Program {
+public class ProgramErp extends VerticalLayout implements Program {
     private static final long serialVersionUID = 1L;
 
-    public ErpProgram() {}
+    public ProgramErp() {}
 
     @Override
     public Component getView() {
