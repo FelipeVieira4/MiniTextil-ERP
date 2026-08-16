94d7a7f09d5e1ab530325166c1499503e201fe5d
FelipeVieira4 <felipevieira58238@gmail.com>
2026-08-16 16:32:22 -0300

    feat: alteração no modelo de Itens

--- src/main/java/com/minitextil/erp/item/model/Item.java
+++ src/main/java/com/minitextil/erp/item/model/Item.java
@@ -1,34 +1,29 @@
 package com.minitextil.erp.item.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
-@Table(name="TBL_ITEM")
+@Table(name = "TBL_ITEM")
 public class Item {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@NotBlank(message = "Descrição do produto obrigátorio!")
 	private String descricao;
-	
+
 	@Column(nullable = false)
-	private boolean ativo=true;
-	
+	private boolean ativo = true;
+
+	@Enumerated(EnumType.STRING)
 	@NotNull(message = "Unidade do produto obrigátorio!")
 	private UnidadeMedida unidade;
 
-	private Item itemPai;
-
-	private VarianteItem varianteItem;
+	//@OneToOne(fetch = FetchType.LAZY)
+	//private ItemSku origemSku;   // nome mais claro: só é preenchido em itens reduzidos
 
 	public Long getId() {
 		return id;
@@ -61,6 +56,14 @@ public class Item {
 	public void setUnidade(UnidadeMedida unidade) {
 		this.unidade = unidade;
 	}
-	
-	
+
+	/*
+	public ItemSku getOrigemSku() {
+		return origemSku;
+	}
+
+	public void setOrigemSku(ItemSku origemSku) {
+		this.origemSku = origemSku;
+	}
+	*/
 }
